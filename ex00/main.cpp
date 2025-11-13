49a41d04355d6907c557a395c5fe841579e01644
Pierre Pontet <ppontet@student.42lyon.fr>
2025-11-13 11:02:43 +0100

    chores: better print + true addresses

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,26 +1,34 @@
 #include "easyfind.hpp"
 
 #include <iostream>
+#include <iomanip>
 #include <vector>
+
+#ifndef DEBUG
+# define DEBUG 1
+#endif
 
 template <typename T>
 void print_vector(const std::vector<T>& vec)
 {
 	typename std::vector<T>::const_iterator start = vec.begin();
 	typename std::vector<T>::const_iterator end = vec.end();
-
+	unsigned int power_of_10 = 0;
+	for (size_t n = end - start; n >= 10; n /= 10)
+		++power_of_10;
 	for (typename std::vector<T>::const_iterator it = start; it != end; ++it)
 	{
-		std::cout << "vec[" << (it - start) << "] = " << *it << std::endl;
+		if (DEBUG)
+			std::cout << "vec[" << std::setw(power_of_10 + 1) << (it - start) << "] = " <<  *it << " -> " << &(*it) << std::endl;
+		else
+			std::cout << "vec[" << std::setw(power_of_10 + 1) << (it - start) << "] = " << *it << std::endl;
 	}
 }
 
 template <typename T>
 void print_size_vector(const std::vector<T>& vec)
 {
-	typename std::vector<T>::const_iterator end = vec.end();
-
-	std::cout << "Vector is from: " << &vec << " -> " << &end << std::endl;
+	std::cout << "Vector data is: " << vec.data() << " -> " << vec.data() + vec.size() - 1 << std::endl;
 	std::cout << "Vector has: " << vec.size() << " values" << std::endl;
 }
 
@@ -28,7 +36,7 @@ int main()
 {
 	std::vector<int> vec;
 
-	for(int i = 0; i < 100; ++i)
+	for(int i = 0; i < 20; ++i)
 		vec.push_back(i * 2);
 
 	print_vector(vec);
@@ -39,7 +47,7 @@ int main()
 
 	try
 	{
-		std::vector<int>::iterator it = easyfind(vec, 5);
+		std::vector<int>::iterator it = easyfind(vec, 6);
 		std::cout << "Found value at: " << &it << " -> " << *it << std::endl;
 	}
 	catch(const std::exception& e)
